eb5a8aa4cb2d5ce32a8837683692a0d41ada6c72
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-04-06 17:08:49 +0530

    Day2 commit

--- src/day2class/Day2javaclass.java
+++ src/day2class/Day2javaclass.java
@@ -3,6 +3,7 @@ package day2class;
 public class Day2javaclass {
     public static void main(String[] args) {
         System.out.println("hello sir");
+        System.out.println("printing name");
     }
 
 }
